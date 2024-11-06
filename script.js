3e7e273cbd01b6c698483cdab4e2b80a4a597030
Daniela <nam991019@gmail.com>
2024-11-05 21:09:12 -0500

    Salida de los datos

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ for (let i = 1; i <= 5; i++) {
             positivos++;
         } else if (numero < 0) {
             negativos++;
-    
+        }
+
+        // La instrucción numero % 15 === 0 verifica si el número es múltiplo de 15. Si el residuo de la división es 0, se incrementa multiplosDe15 en 1.
+        if (numero % 15 === 0) {
+            multiplosDe15++;
+        }
+    }
+
+ // Una vez terminado el bucle, el programa muestra los resultados en la consola con document.write, indicando las cantidades de números negativos, positivos y múltiplos de 15.
+document.write("Cantidad de valores negativos ingresados: " + negativos + "<br>");
+document.write("Cantidad de valores positivos ingresados: " + positivos + "<br>");
+document.write("Cantidad de múltiplos de 15 ingresados: " + multiplosDe15 + "<br>");
